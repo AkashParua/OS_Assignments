839cfb4484c9c9ffd00e6d331adef3d7b8a08859
AkashParua <akashparua999@gmail.com>
2022-04-04 14:21:22 +0530

    added

---
+++ Ass5/kernel.cpp
@@ -0,0 +1,15 @@
+//Program to simulate a simple CPU
+//Note : We have only used Virtual Memory to reduce complexity of mapping between physical and virtual memory
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+struct page
+{
+
+}
+int main()
+{
+
+}
+
